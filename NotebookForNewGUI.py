6eda3718dc944d768b1efc05f736278fe8cc17a7
Kaniyah33 <81383676+Kaniyah33@users.noreply.github.com>
2023-11-03 22:10:04 -0400

    updated year, scrollbar, temporary text
    Provides examples for temporary text in user entry boxes, allows for start and end years to be calculated, lets the user scroll down the treeview. Scrollbar has visibility issues but functions

--- NotebookForNewGUI.py
+++ NotebookForNewGUI.py
@@ -62,8 +62,9 @@ from ADSsearcherpkg import *
 import csv
 
 my_gui = tk.Tk()
-my_gui.geometry("1600x350")  # width x height
+my_gui.resizable(width = 1600, height = 350)#geometry("1600x350")  # width x height
 my_gui.title("ADSsearch.com")  # Adding a title
+style = ttk.Style() #creates a style object
 
 searchButton = tk.Button(my_gui, text="Search", width=7, font=18, command=lambda: my_search()) # search button
 searchButton.grid(row=1, column=3)
@@ -72,59 +73,122 @@ authLab = tk.Label(my_gui, text="Author name:", width=15, font=18)  #author name
 authLab.grid(row=1, column=1)
 
 authEnt = tk.Entry(my_gui, width=20, bg="light gray", font=18)  #author name entry box
+authEnt.insert(0, "ex. Last, First")
 authEnt.grid(row=1, column=2)
+authEnt.bind("<FocusIn>", lambda event: authEnt.delete(0,"end") if authEnt.get() == "ex. Last, First" else None)
+authEnt.bind("<FocusOut>", lambda event: authEnt.insert(0, "ex. Last, First") if authEnt.get() == "" else None)
+
 
 instLab = tk.Label(my_gui, text="Institution name:", width=15, font=18)  #institution name label
 instLab.grid(row=2, column=1)
 
 instEnt = tk.Entry(my_gui, width=20, bg="light gray", font=18)  #institution name entry box
+instEnt.insert(0, "ex. Example University")
 instEnt.grid(row=2, column=2)
+instEnt.bind("<FocusIn>", lambda event: instEnt.delete(0,"end") if instEnt.get() == "ex. Example University" else None)
+instEnt.bind("<FocusOut>", lambda event: instEnt.insert(0, "ex. Example University") if instEnt.get() == "" else None)
 
 startLab = tk.Label(my_gui, text="start year:", width=7, font=18)  #start year label
 startLab.grid(row=3, column=1)
 
 startEnt = tk.Entry(my_gui, width=20, bg="light gray", font=18)  # added one Entry box
+startEnt.insert(0, "ex. XXXX")
 startEnt.grid(row=3, column=2)
+startEnt.bind("<FocusIn>", lambda event: startEnt.delete(0,"end") if startEnt.get() == "ex. XXXX" else None)
+startEnt.bind("<FocusOut>", lambda event: startEnt.insert(0, "ex. XXXX") if startEnt.get() == "" else None)
+
+endLab = tk.Label(my_gui, text="end year:", width=7, font=18)  #start year label
+endLab.grid(row=4, column=1)
+
+endEnt = tk.Entry(my_gui, width=20, bg="light gray", font=18)  # added one Entry box
+endEnt.insert(0, "ex. XXXX")
+endEnt.grid(row=4, column=2)
+endEnt.bind("<FocusIn>", lambda event: endEnt.delete(0,"end") if endEnt.get() == "ex. XXXX" else None)
+endEnt.bind("<FocusOut>", lambda event: endEnt.insert(0, "ex. XXXX") if endEnt.get() == "" else None)
 
 #(name=None, institution=None, year= None, refereed= 'property:notrefereed OR property:refereed', \
                #token=None, stop_dir=None):
-
+        
+        
 def my_search():
     value = 0
     if len(authEnt.get()) == 0: #check for author name
         author = None
+    elif authEnt.get() == "ex. Last, First": #if name is example
+        author = None
     else:
         value += 1
         author = authEnt.get()
+        
+        
     if len(instEnt.get()) == 0: #check for institution name
+        institute = None
+    elif instEnt.get() == "ex. Example University": #if institution is example
         institute = None
     else:
         value += 2
         institute = instEnt.get()
-    if len(startEnt.get()) == 0: # or (len(endEnt.get()) != 4): #check for start and end year
+        
+        
+    if len(startEnt.get()) == 0:  #check for start and end year, if 0
         yr = None
-    elif startEnt.get().isdigit() == False:
-        yr = None
-    else: 
-        value += 4
-        if len(startEnt.get()) == 4:
-            yr = startEnt.get()
-        else:
+        showinfo(
+        title='Error',
+        message='You must insert numbers.'
+    )
+    elif (startEnt.get() == "ex. XXXX") and (endEnt.get() == "ex. XXXX"): #if the years are the examples, if empty:
+            value += 4
             yr = "[2000 to 2023]"
+    elif ((startEnt.get().isdigit() == False) and (startEnt.get() != "ex. XXXX")) or ((endEnt.get().isdigit() == False) and (endEnt.get() != "ex. XXXX")) : #if a year is not a digit and not the example
+        yr = None
+        showinfo(
+        title='Error',
+        message='You must insert numbers.'
+    )
+    elif ((len(startEnt.get()) != 4) and (startEnt.get() != "ex. XXXX")) or ((len(endEnt.get()) != 4) and (endEnt.get() != "ex. XXXX")): #if year is not 4 numbers and not the example
+        yr = None
+        showinfo(
+        title='Error',
+        message='Please check your formatting.'
+    )
+    else:
+        value += 4
+        if (len(startEnt.get()) == 4) and (len(endEnt.get()) == 4): #combines if the years are digits and a length of 4
+            yr = "[" + startEnt.get() + " TO " + endEnt.get() + "]"
+        elif (len(startEnt.get()) == 4) and (endEnt.get() == "ex. XXXX"): #if its just the start year
+            yr = startEnt.get()
+        elif (len(endEnt.get()) == 4) and (startEnt.get() == "ex. XXXX"): #if its just the end year
+            yr = endEnt.get()    
+        else: 
+            yr = "[2000 to 2023]"
+        print(yr)
         
-        # if the len == 4: 
-            #yr = startEnt.get()
-            
-        #if the len is 4
-        #if int
-        #if empty
-        #if longer than 4
+    #if year ==:
+        #start: "" end: ""       if empty: 2000 to 2023
+        #start: xxxx end: xxxx   if all xxxx: 2000 to 2023
+        #start: xxxx end: 2023   if start year is xxxx: yr = endEnt.get()
+        #start: 2023 end: xxxx   if end year is xxxx: yr = startEnt.get()
+        #start: 2023 end: 2023   if both correct: yr = [startEnt.get() TO endEnt.get()]
         
-        #print(type(startEnt.get())) #type string, get it to type int
+        #start: xxxx end: 20033  if len(startEnt.get()) != 4 or len(endEnt.get()) != 4: print error
+        #start: 203 end: 20033
+        #start: 2023 end: 20033
+        #start: 20033 end: 20033
+        #start: 20033 end: xxxx
+        #start: 20033 end: 203
+        #start: 20033 end: 2023
         
-    # if value == 1:
-    # mirror mallory's code, for 2,3, 5, 6,7 and well
-    #access just the function and the value
+        #start: "2023" end: xxxx if (startEnt.get().isdigit == False) or (endEnt.get().isdigit == False): print error
+        #start: "2023" end: 2023
+        #start: "2023" end: 203
+        #start: "2023" end: 20033
+        
+        #start: xxxx end: "2023" 
+        #start: 2023 end: "2023"
+        #start: 203 end: "2023"
+        #start: 20033 end: "2023"
+        #start: "2023" end: "2023"
+        
     
     print("value: ", value)
     if value == 1:
@@ -135,7 +199,7 @@ def my_search():
         datf=AP.ads_search(name= author, institution= institute ,
                token=token, stop_dir=stop_dir) #creates dataframe
     elif value == 4:
-        #datf = datf = AP.ads_search(year = yr, token = token, stop_dir = stop_dir)
+        #datf = AP.ads_search(year = yr, token = token, stop_dir = stop_dir)
         showinfo(
         title='Error',
         message='You did not give me enough to search on, please try again.'
@@ -156,8 +220,8 @@ def my_search():
     datf["Title"] = datf["Title"].str.wrap(70)
     datf['Affiliations'] = datf['Affiliations'].str.wrap(30)
     df2 = datf
-    #print(type(datf), datf.head())
-    l1 = list(datf) # List of column names as headers
+    l1 = list(datf)
+    print(l1)# List of column names as headers
     r_set = datf.to_numpy().tolist()  # Create list of list using rows
     trv = ttk.Treeview(my_gui, selectmode="browse")  # selectmode="browse" or "extended"
     trv.grid(row=5, column=1, columnspan=4, padx=10, pady=20)  #
@@ -172,16 +236,29 @@ def my_search():
         trv.heading(i, text=i)
     for dt in r_set:
         v = [r for r in dt]  # creating a list from each row
-        #print(v[0])
+        #print(v)
         try:
             trv.insert("", "end", iid=v[0], values=v)  # adding row
         except:
             continue
         #if id value already in chart, find a way to bypass it
-    
-    saveButton = tk.Button(my_gui, text="Save CSV", bg="blue", width=10, font=18, command=lambda:save_to_csv())
+    verscrlbar = ttk.Scrollbar(my_gui, 
+                           orient ="vertical", 
+                           command = trv.yview)
+    verscrlbar.grid(row= 5, column=0, sticky='NS')#(side ='right', fill ='x')
+    # Configure the style for the scrollbar
+    style.configure("Treeview.Scrollbar",
+                background="blue",
+                troughcolor="black",
+                gripcount=0,
+                gripcolor="white",
+                gripinset=2,
+                gripborderwidth=0,
+                thickness=10)
+    trv.configure(xscrollcommand = verscrlbar.set)
+    saveButton = tk.Button(my_gui, text="Save CSV", bg="blue", width=10, font=18, command= lambda:save_to_csv())
     saveButton.grid(row=2, column=3)
-
+    
 def save_to_csv():
     global df2
     if df2 is not None:
@@ -192,9 +269,8 @@ def save_to_csv():
             print(f"Data saved to {file_path}")
     else:
         print("DataFrame is not available, you must create it first.")
-      
+    
 my_gui.mainloop()
-
 
 # In[ ]:
 
